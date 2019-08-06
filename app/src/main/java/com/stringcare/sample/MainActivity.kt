4711005c4a34f039aa388939066e40e5aa2836a7
Efra Espada <efraespada@gmail.com>
2019-08-06 19:06:33 +0200

    kotlin sample

--- app/src/main/java/com/stringcare/sample/MainActivity.kt
+++ app/src/main/java/com/stringcare/sample/MainActivity.kt
@@ -3,6 +3,7 @@ package com.stringcare.sample
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.stringcare.library.SC
+import com.stringcare.library.reveal
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,8 +13,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         SC.init { applicationContext }
 
-        kenobi.text = SC.reveal(R.string.kenobi)
-        r2d2.text = SC.reveal(R.string.r2d2)
-        c3po.text = SC.reveal(R.string.c3po, false)
+        kenobi.text = R.string.kenobi.reveal()
+        r2d2.text = R.string.r2d2.reveal()
+        c3po.text = R.string.c3po.reveal(false)
     }
 }

61b1c089e5a10f325d7e09a6ef891c94c1435120
Efra Espada <efraespada@gmail.com>
2019-08-17 22:17:32 +0200

    layout update

--- app/src/main/java/com/stringcare/sample/MainActivity.kt
+++ app/src/main/java/com/stringcare/sample/MainActivity.kt
@@ -2,8 +2,7 @@ package com.stringcare.sample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import com.stringcare.library.SC
-import com.stringcare.library.reveal
+import com.stringcare.library.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -16,5 +15,12 @@ class MainActivity : AppCompatActivity() {
         kenobi.text = R.string.kenobi.reveal()
         r2d2.text = R.string.r2d2.reveal()
         c3po.text = R.string.c3po.reveal(false)
+
+        "test.json".asyncJson { json ->
+            tv_json.text = json.toString()
+        }
+        "test.json".asyncBytes({ false }) { bytes ->
+            tv_json_original.text = String(bytes)
+        }
     }
 }

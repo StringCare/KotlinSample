894c7707d7bab2bf18dbe2442d49279345cd9a9e
Efra Espada <efraespada@gmail.com>
2020-06-30 09:32:59 +0200

    update to AndroidX

--- app/src/main/java/com/stringcare/sample/MainActivity.kt
+++ app/src/main/java/com/stringcare/sample/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.stringcare.sample
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.stringcare.library.*
 import kotlinx.android.synthetic.main.activity_main.*
 
